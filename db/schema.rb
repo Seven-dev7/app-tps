fdc61793151e5274596e7c6a411f8f57bc28ba88
jpmagido <jpmagido@me.com>
2020-05-19 23:23:19 +0200

    update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_180821) do
+ActiveRecord::Schema.define(version: 2020_05_19_170323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,21 +18,21 @@ ActiveRecord::Schema.define(version: 2020_05_19_180821) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "idcard"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "vitalcard"
+    t.string "driverlicence"
+    t.string "zipcode"
+    t.string "dateofbirth"
+    t.string "phone"
+    t.string "adress"
+    t.string "city"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "idcard"
-    t.string "vitalcard"
-    t.string "driverlicence"
-    t.string "dateofbirth"
-    t.string "zipcode"
-    t.string "phone"
-    t.text "adress"
-    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
