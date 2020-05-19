bde8d4a95d2c7c6535858d858ac77b145670d7f8
jpmagido <jpmagido@me.com>
2020-05-19 23:21:55 +0200

    change controller + db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
 
-    protected
+  def configure_permitted_parameters
+    parameters = %i(first_name last_name phone zipcode city adress idcard driverlicence vitalcard dateofbirth email password current_password password_confirmation)
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(parameters) }
+  end
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :phone, :zipcode, :city, :adress, :idcard, :driverlicence, :vitalcard, :dateofbirth, :email, :password, :current_password) }
-    end
+  def after_sign_in_path_for(resource)
+    private_path
+  end
 
-	def after_sign_in_path_for(resource)
-  		private_path
-	end
-	def after_update_path_for(resource)
-		private_path(resource)		
-	end
+  def after_update_path_for(resource)
+    private_path(resource)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   root to: "home#index"
 
 
-get "/private", to: "home#private"
+  get "/private", to: "home#private"
 
 end
